f1eb938c6febe2506d49cf5e8d1de1e6f819d5c8
mafkar2 <mafkar2@gmail.com>
2024-04-20 07:24:30 -0700

    updating npm

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -10,3 +10,5 @@ function Header(props) {
     </header>
   );
 }
+
+export default Header;
---
+++ client/src/components/ProjectCard/index.jsx
@@ -0,0 +1,29 @@
+import { removeHyphensAndCapitalize } from '../../utils/helpers';
+import * as projects from '../../assets';
+
+
+function ProjectCard({ project }) {
+  const { name, repo, link, description } = project;
+
+  return (
+    <div className="project" key={name}>
+      <img
+        src={projects[name]}
+        alt={removeHyphensAndCapitalize(name)}
+        className="project-bg"
+      />
+      <div className="project-text">
+        <h3>
+          <a href={link}>{removeHyphensAndCapitalize(name)}</a>
+          &nbsp;
+          <a href={repo}>
+            <i className="fab fa-github" />
+          </a>
+        </h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
